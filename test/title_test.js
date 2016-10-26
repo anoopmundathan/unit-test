2ead85e58f82586573d8ab347eb9633e017a62a5
Anoop Mundathan <anoopmundathan@Anoops-MacBook-Pro.local>
2016-10-26 18:19:46 +0100

    Added new test

--- test/title_test.js
+++ test/title_test.js
@@ -6,6 +6,7 @@ describe('Mocha', function () {
 
 	it('It should convert into title case', function () {
 		expect(title.titleCase('a')).to.equal('A');
+		expect(title.titleCase('a')).to.equal('B');
 		expect(title.titleCase('hello world')).to.equal('Hello World');
 		expect(title.titleCase('the great expectation')).to.equal('The Great Expectation');
 	});
--- title.js
+++ title.js
@@ -1,10 +1,10 @@
 module.exports.titleCase = function (title) {
+
 	var words = title.split(' ');
+
 	var titleWords = words.map(function(word) {
 		return word[0].toUpperCase() + word.substring(1);
 	});
 
 	return titleWords.join(' ');
 }
-
-// module.exports.titleCase = titleCase;
